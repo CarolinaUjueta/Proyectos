d132fbbf436ff7793764dfbf2875803a97186ac7
Carosis <c_ujueta@javeriana.edu.co>
2025-10-29 21:44:25 -0500

    Add headers.c with function prototypes
    Define function prototypes for reading integers and summing an array.

---
+++ SO_projects/Taller_Fork/headers.c
@@ -0,0 +1,26 @@
+/*****************************************************************************************************
+ *                                   PONTIFICIA UNIVERSIDAD JAVERIANA                                *
+ *                     Departamento de Ingeniería de Sistemas – Sistemas Operativos                  *
+ *                                                                                                   *
+ * ------------------------------------------------------------------------------------------------- *
+ * Autor       : Thomas Leal, Carolina Ujueta                                                        *
+ * Fecha       : 27/10/2025                                                                          *
+ * Objetivo    : 4 procesos con fork(); cada hijo lee su archivo y calcula la suma total de enteros. *
+ *               Padre recibe por pipe el resultado.                                                 *
+ *                                                                                                   *
+ *****************************************************************************************************
+ * En este archivo se encuentran definidas las funciones implementadas en la lógica del codigo       *
+ *                                                                                                   *
+ *****************************************************************************************************/
+
+/************************************************* Headers **************************************************/
+#ifndef _HEADERS_H
+#define _HEADERS_H
+
+#include <stddef.h>
+
+/* ---- Prototipos ---- */
+int *leer_enteros(char *ruta, size_t n);
+int   sumar_arreglo(int *v, size_t n);
+
+#endif
