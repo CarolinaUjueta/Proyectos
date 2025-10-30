19d02df8e4506ba6264e4cdccd481338e3791850
Carosis <c_ujueta@javeriana.edu.co>
2025-10-29 21:47:07 -0500

    Remove unused function prototypes in funciones.c
    Removed function prototypes for leer_enteros and sumar_arreglo.

--- SO_projects/Taller_Fork/funciones.c
+++ SO_projects/Taller_Fork/funciones.c
@@ -1,3 +1,17 @@
+/*****************************************************************************************************
+ *                                   PONTIFICIA UNIVERSIDAD JAVERIANA                                *
+ *                     Departamento de Ingeniería de Sistemas – Sistemas Operativos                  *
+ *                                                                                                   *
+ * ------------------------------------------------------------------------------------------------- *
+ * Autor       : Thomas Leal, Carolina Ujueta                                                        *
+ * Fecha       : 27/10/2025                                                                          *
+ * Objetivo    : 4 procesos con fork(); cada hijo lee su archivo y calcula la suma total de enteros. *
+ *               Padre recibe por pipe el resultado.                                                 *
+ *                                                                                                   *
+ *****************************************************************************************************
+ * En este archivo estan implementados los prototipos de las funciones que se pusieron en funciones.h*
+ *****************************************************************************************************/
+
 /************************************************* Headers **************************************************/
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,10 +19,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-
-/* ---- Prototipos ---- */
-int *leer_enteros(char *ruta, size_t n);
-int   sumar_arreglo(int *v, size_t n);
 
 /************************************************************************************************************/
 /*                                                                                                          */
