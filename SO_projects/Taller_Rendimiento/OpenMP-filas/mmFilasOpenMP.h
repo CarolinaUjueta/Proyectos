c89d7228ae4588fab2739fb08416b87c1ef66861
Caro Ujueta <c_ujueta@javeriana.edu.co>
2025-11-13 12:36:20 -0500

    Create mmFilasOpenMP.h

---
+++ SO_projects/Taller_Rendimiento/OpenMP-filas/mmFilasOpenMP.h
@@ -0,0 +1,28 @@
+/************************************************************************************************************/
+/*  ARCHIVO: mmTransOpenMP.h                                                                                */
+/*  DESCRIPCIÓN:                                                                                            */
+/*  Declaraciones, estructuras y prototipos utilizados en la implementación del algoritmo                    */
+/*  de multiplicación de matrices transpuestas con paralelismo OpenMP.                                      */
+/************************************************************************************************************/
+
+#ifndef MM_TRANS_OPENMP_H
+#define MM_TRANS_OPENMP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <omp.h>
+
+/* Variables globales para medición */
+extern struct timeval inicio, fin;
+
+/* Prototipos de funciones */
+void InicioMuestra();
+void FinMuestra();
+void iniMatrix(double *m1, double *m2, int D);
+void impMatrix(double *matrix, int D, int t);
+void multiMatrixTrans(double *mA, double *mB, double *mC, int D);
+
+#endif
